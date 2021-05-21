7ed28442381700f80a1d3f5e2bc68dba11bbdb8e
Florin88 <>
2021-05-21 15:56:24 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+ function countdown(){
+  console.log(7)
+  console.log(6)
+  console.log(5)
+  console.log(4)
+  console.log(3)
+  console.log(2)
+  console.log(1)
+}
+
+countdown()
+
+let button1 = "buttonUp"
+console.log("button1")
+let button2 = "buttonDown"
+console.log("button2")
